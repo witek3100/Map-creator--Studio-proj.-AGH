2c2a5a5bdb6e9e3ecc10d48d54fbe285bc162811
witek <witek.nowogorski@gmail.com>
2023-11-24 15:06:03 +0100

    .

--- src/ml/datasets/transforms.py
+++ src/ml/datasets/transforms.py
@@ -3,11 +3,15 @@ from PIL import Image
 
 
 train_transforms = transforms.Compose([
-    transforms.ColorJitter(brightness=[0.5, 1.5], contrast=[0.8, 1.2], saturation=[0.8, 1.2]),
-    transforms.RandomHorizontalFlip(),
+    transforms.RandomHorizontalFlip(p=0.5),
+    transforms.RandomVerticalFlip(p=0.5),
+#     transforms.GaussianBlur(kernel_size=(5, 9), sigma=(0.1, 5)),
+    transforms.RandomRotation(degrees=(30, 70)),
     transforms.ToTensor(),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
 test_val_transforms = transforms.Compose([
     transforms.ToTensor(),
+    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
